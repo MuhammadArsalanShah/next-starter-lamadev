37b8eb17f59b67b530a4a180fbb6dd3270eed393
MuhammadArsalanShah <arsalanshah408@yahoo.com>
2024-09-13 16:39:00 +0500

    Finishing footer component styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,12 @@
+import styles from "./footer.module.css";
+
 const Footer = () => {
-  return <div>This is Footer</div>;
+  return <div className={styles.container}>
+    <div className={styles.logo}>lamadev</div>
+    <div className={styles.text}>
+      Lama creative thoughts agency &copy; All rights reserved
+    </div>
+  </div>;
 };
 
 export default Footer;
