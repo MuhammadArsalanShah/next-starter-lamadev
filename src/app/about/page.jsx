66529d8bbfd063a6dafc409866e063461161b99e
MuhammadArsalanShah <arsalanshah408@yahoo.com>
2024-09-13 15:52:20 +0500

    1. Adding images in public folder 2. understanding next.js Image component and also understanding how to insert images from the internet

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const AboutPage = () => {
-  return <div>About Page</div>;
+  return (
+    <div>
+      <h1>About Page</h1>
+      <div className={styles.imageContainer}>
+        {/* <Image src="/about.png" alt="" fill /> */}
+        <Image src="https://images.pexels.com/photos/28209723/pexels-photo-28209723/free-photo-of-women-overlooking-city-at-night.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" fill />
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
