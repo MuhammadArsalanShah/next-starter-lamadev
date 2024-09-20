5a53345c6d46d4cbbfe10e7ad13f5142524ce372
MuhammadArsalanShah <arsalanshah408@yahoo.com>
2024-09-20 16:04:08 +0500

    authorization based on user logged in and based on isAdmin property completed

--- src/lib/auth.config.js
+++ src/lib/auth.config.js
@@ -4,8 +4,46 @@ export const authConfig = {
   },
   providers: [],
   callbacks: {
+    async jwt({token, user}){
+      if (user) {
+        token.id = user.id;
+        token.username = user.username;
+        token.isAdmin = user.isAdmin;
+      }
+      return token;
+    },
+    async session({session, token}){
+      if (token) {
+        session.user.id = token.id;
+        session.user.username = token.username;
+        session.user.isAdmin = token.isAdmin;
+      }
+      return session;
+    },
     authorized({auth, request}) {
-      return false;
+      // console.log("My Auth:", auth);
+      
+      const user = auth?.user;
+      const isOnAdminPanel = request.nextUrl?.pathname.startsWith("/admin");
+      const isOnBlogPage = request.nextUrl?.pathname.startsWith("/blog");
+      const isOnLoginPage = request.nextUrl?.pathname.startsWith("/login");
+
+      // ONLY ADMIN CAN REACH THE ADMIN DASHBOARD
+      if (isOnAdminPanel && !user?.isAdmin) {
+        return false;
+      }
+
+      // ONLY AUTHENTICATED USERS CAN REACH BLOG PAGE
+      if (isOnBlogPage && !user) {
+        return false;
+      }
+
+      // ONLY UNAUTHENTICATED USERS CAN REACH THE LOGIN PAGE
+      if (isOnLoginPage && user) {
+        return Response.redirect(new URL("/", request.nextUrl));
+      }
+
+      return true;
     },
   }
 }
